2a98244b069ec7036decdaae7700a557121d01dd
marKinhuENTI <95031286+Koaladelamor@users.noreply.github.com>
2022-11-30 16:46:05 +0100

    el pusheito

---
+++ Assets/Scripts/DraggableItemClase.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+
+public class DraggableItemClase : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private GameObject mainCanvas;
+
+    private void Awake()
+    {
+        mainCanvas = GameObject.FindGameObjectWithTag("MainCanvas");
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("onBeginDrag");
+        MouseData.tempItemBeingDragged = Instantiate(this.gameObject, mainCanvas.transform);
+        MouseData.tempItemBeingDragged.GetComponent<CanvasGroup>().blocksRaycasts = false;
+        MouseData.tempItemBeingDragged.GetComponentInChildren<TextMeshProUGUI>().alignment = TextAlignmentOptions.Center;
+        //MouseData.tempItemBeingDragged.GetComponent<Image>().raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("onDrag");
+        if (MouseData.tempItemBeingDragged != null)
+        {
+            RectTransform rectTransform = MouseData.tempItemBeingDragged.GetComponent<RectTransform>();
+            rectTransform.anchoredPosition = Input.mousePosition;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("onEndDrag");
+        if (MouseData.mouseOverPanelTransform != null)
+        {
+            if (MouseData.mouseOverPanelTransform.name == "pnl_Crafting")
+            {
+                GameObject slotAvailable = MouseData.mouseOverPanelTransform.GetComponent<CraftManager>().FirstSlotAvailable();
+                if (slotAvailable != null)
+                {
+
+                    slotAvailable.GetComponent<TextMeshProUGUI>().text = MouseData.tempItemBeingDragged.GetComponentInChildren<TextMeshProUGUI>().text;
+                    slotAvailable.GetComponent<TextMeshProUGUI>().text = MouseData.tempItemBeingDragged.GetComponentInChildren<TextMeshProUGUI>().text;
+                    slotAvailable.GetComponent<IngredientInfo>().SetID(MouseData.tempItemBeingDragged.GetComponent<IngredientInfo>().GetID());
+                    slotAvailable.GetComponent<IngredientInfo>().SetIngredientName(MouseData.tempItemBeingDragged.GetComponent<IngredientInfo>().GetIngredientName());
+                    Destroy(MouseData.tempItemBeingDragged);
+                    MouseData.tempItemBeingDragged = null;
+                }
+                else
+                {
+                    Destroy(MouseData.tempItemBeingDragged);
+                    MouseData.tempItemBeingDragged = null;
+                }
+            }
+            else
+            {
+                Destroy(MouseData.tempItemBeingDragged);
+                MouseData.tempItemBeingDragged = null;
+            }
+        }
+        //tempItemBeingDragged.transform.SetParent(parentAfterDragEnd);
+    }
+
+}
